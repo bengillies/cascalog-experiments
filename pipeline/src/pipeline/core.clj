ed997a528e2434bbc2fbe3b890aba78221e8f578
Ben Gillies <bengillies@gmail.com>
2012-12-03 17:30:57 +0000

    Added comments and cleaned up old functions

--- pipeline/src/pipeline/core.clj
+++ pipeline/src/pipeline/core.clj
@@ -9,10 +9,6 @@
   (:import [java.util Calendar]
            [java.text SimpleDateFormat]))
 
-;; because...
-(defmacro log [& msg]
-  `(println (join " " '~msg)))
-
 ;; define an input and output tap
 
 (defn csv-tap [path]
@@ -32,12 +28,14 @@
       (reduce #(assoc %1 %2 (get (%1 %2))) mapping (keys mapping)))))
 
 
+;; locations of expected data
 (def grant-in "../data/grants_epsrc_grants_1.csv")
 (def department-in "../data/departments_epsrc_grants_1.csv")
 (def organisation-in "../data/organisations_epsrc_grants_1.csv")
 
 
 (defn get-year [date-str]
+  "convert a date string into a year"
   (let [cal (Calendar/getInstance)
         date (.parse (SimpleDateFormat. "dd MMMMM yyyy") date-str)]
     (do
@@ -48,6 +46,7 @@
 ;; XXX: this is ugly but cascalog makes us defn every function explicitly
 ;; (i.e. we can't generate this function using another function)
 (defn to-grant [row]
+  "convert a row from the grant csv into a hashmap"
   ((transform-row {:id 0
                   :ref 1
                   :title 2
@@ -56,32 +55,17 @@
                   :value 7
                   :desc 8}) row))
 
-(defn grant-query [path-in path-out]
-  (?<- (output-tap path-out) [?grant]
-       ((csv-tap path-in) ?row)
-       (to-grant ?row :> ?grant)))
-
 ;; department data
 (defn to-department [row]
+  "convert  row from the department csv into a hashmap"
   ((transform-row {:organisation_id 0
                    :name 1
                    :id 2}) row))
 
-(defn department-query [path-in path-out]
-  (?<- (output-tap path-out) [?dep]
-       ((csv-tap path-in) ?row)
-       (to-department ?row :> ?dep)))
-
-
 ;; organisation data
 (defn to-organisation [row]
+  " convert a row from the organisation csv into a hashmap"
   ((transform-row {:name 0 :id 1}) row))
-
-(defn organisation-query [path-in path-out]
-  (?<- (output-tap path-out) [?org]
-       ((csv-tap path-in) ?row)
-       (to-organisation ?row :> ?org)))
-
 
 ;; define some intermediate queries
 ;;
@@ -89,44 +73,69 @@
 ;; sum up all the values per organisation
 
 (defn get-grant-values [n]
+  "extract the values we need from the grant data"
   [(Long. (n :value))
    (n :id)
    (get-year (n :start-date))])
-(def grant-values (<- [?value ?departmentid ?year]
-                      ((csv-tap grant-in) ?row)
-                      (to-grant ?row :> ?grant)
-                      (get-grant-values ?grant :> ?value ?departmentid ?year)))
 
-(defn get-department-ids [n] [(n :id) (n :organisation_id)])
-(def departments (<- [?departmentid ?organisationid]
-                     ((csv-tap department-in) ?row)
-                     (to-department ?row :> ?department)
-                     (get-department-ids ?department :>
-                                         ?departmentid ?organisationid)))
+(def grant-values
+  "query grant data for value, departmentid and year grant started"
+  (<- [?value ?departmentid ?year]
+      ((csv-tap grant-in) ?row)
+      (to-grant ?row :> ?grant)
+      (get-grant-values ?grant :> ?value ?departmentid ?year)))
 
-(defn get-organisation-values [n] [(n :id) (n :name)])
-(def organisations (<- [?id ?name]
-                       ((csv-tap organisation-in) ?row)
-                       (to-organisation ?row :> ?organisation)
-                       (get-organisation-values ?organisation :> ?id ?name)))
 
-(def department-to-organisation (<- [?departmentid ?orgname]
-                                    (departments ?departmentid ?orgid)
-                                    (organisations ?orgid ?orgname)))
+(defn get-department-ids [n]
+  "extract the values we need from the department data"
+  [(n :id) (n :organisation_id)])
 
-(def grants-plus-orgs (<- [?value ?orgname ?year]
-                          (grant-values ?value ?departmentid ?year)
-                          (department-to-organisation ?departmentid ?orgname)))
+(def departments
+  "query department data to map department ids to organisation ids"
+  (<- [?departmentid ?organisationid]
+      ((csv-tap department-in) ?row)
+      (to-department ?row :> ?department)
+      (get-department-ids ?department :>
+                          ?departmentid ?organisationid)))
 
-(def sum-grants (<- [?orgname ?total ?year]
-                    (grants-plus-orgs ?value ?orgname ?year)
-                    (sum ?value :> ?total)))
 
-(defn pair [tuple]
-  {:total (tuple :total)
-   :year (tuple :year)})
+(defn get-organisation-values [n]
+  "extract the values we need from the organisation data"
+  [(n :id) (n :name)])
 
-(defbufferop group-totals [tuples]
+(def organisations
+  "query organisation data for id and name"
+  (<- [?id ?name]
+      ((csv-tap organisation-in) ?row)
+      (to-organisation ?row :> ?organisation)
+      (get-organisation-values ?organisation :> ?id ?name)))
+
+
+(def department-to-organisation
+  "convert deprtment ids to organisation names"
+  (<- [?departmentid ?orgname]
+      (departments ?departmentid ?orgid)
+      (organisations ?orgid ?orgname)))
+
+
+(def grants-plus-orgs
+  "swap department ids from grant-values with organisation names"
+  (<- [?value ?orgname ?year]
+      (grant-values ?value ?departmentid ?year)
+      (department-to-organisation ?departmentid ?orgname)))
+
+
+(def sum-grants
+  "sum value fields per organisation/year"
+  (<- [?orgname ?total ?year]
+      (grants-plus-orgs ?value ?orgname ?year)
+      (sum ?value :> ?total)))
+
+
+(defbufferop group-totals
+             "reduce tuples into a list grouping totals and years by
+             organisation"
+             [tuples]
              (let [orgs (group-by first tuples)]
                (map (fn [[org tuple-list]]
                       {:organisation org
@@ -136,20 +145,16 @@
                                           tuple-list))})
                     orgs)))
 
-(defn sum-grants-to-obj [org total year]
-  {:organisation org
-   :total total
-   :year year})
 
 (defn run-queries []
-  "run all the specified queries"
+  "Calculate total grants per year per organisation"
   (?<- (output-tap "../foosite/data/totals") [?totals]
        (sum-grants ?orgname ?total ?year)
        (group-totals ?orgname ?total ?year :> ?totals)))
 
 (defn -main [& m]
   (bootstrap)
-  (log started Hadoop)
+  (println "started Hadoop")
   (run-queries)
-  (log finished processing grant data))
+  (println "finished processing grant data"))
 
